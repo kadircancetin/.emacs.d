cb83cd84560b465615aeccb34f43a72bfa9ba406
kadir can çetin <kadircancetin@gmail.com>
2020-01-09 15:37:13 +0300

    [Fix] Removed init with tangeld then no need tangle hook

--- init.el
+++ init.el
@@ -34,17 +34,17 @@
 
 (defun emacs_start__with_tangled()
   (progn
-    (defun my/tangle-dotfiles ()
-      ;; source: https://emacs.stackexchange.com/questions/29214/org-based-init-method-slows-down-emacs-startup-dramaticlly-6-minute-startup-h
-      "If the current file is this file, the code blocks are tangled"
-      (when (equal (buffer-file-name) (expand-file-name "~/.emacs.d_kadir/README.org"))
-        (org-babel-tangle nil "~/.emacs.d_kadir/README.el")
-        ;; (byte-compile-file "~/.emacs.d/README.el")
-        ))
-    (add-hook 'after-save-hook #'my/tangle-dotfiles)
-    (load-file "~/.emacs.d_kadir/README.el")))
+    ;; (defun my/tangle-dotfiles ()
+    ;;   ;; source: https://emacs.stackexchange.com/questions/29214/org-based-init-method-slows-down-emacs-startup-dramaticlly-6-minute-startup-h
+    ;;   "If the current file is this file, the code blocks are tangled"
+    ;;   (when (equal (buffer-file-name) (expand-file-name "~/.emacs.d/README.org"))
+    ;;     (org-babel-tangle nil "~/.emacs.d/README.el")
+    ;;     ;; (byte-compile-file "~/.emacs.d/README.el")
+    ;;     ))
+    ;; (add-hook 'after-save-hook #'my/tangle-dotfiles)
+    (load-file "~/.emacs.d/README.el")
+    ))
 
-
-(if (most-recent-p  "~/.emacs.d_kadir/README.org" "~/.emacs.d_kadir/README.el")
+(if (most-recent-p  "~/.emacs.d/README.org" "~/.emacs.d/README.el")
     (emacs_start__with_tangled)
   (emacs_start__with_org))
