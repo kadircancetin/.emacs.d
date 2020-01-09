e252c59428dd5d58b8d680794fa45aba31670851
kadir can çetin <kadircancetin@gmail.com>
2020-01-09 15:26:09 +0300

    [Add] if org new open with org else, open with tangled file

--- init.el
+++ init.el
@@ -1,10 +1,30 @@
-(setq emacs_start_from nil)             ; if nil from ORG, otherwise file
-(setq emacs_start_from 'file)           ; if nil from ORG, otherwise file
+;; (use-package auto-package-update
+;;   :config
+;;   ;; Delete residual old versions
+;;   (setq auto-package-update-delete-old-versions t)
+;;   ;; Do not bother me when updates have taken place.
+;;   (setq auto-package-update-hide-results t)
+;;   ;; Update installed packages at startup if there is an update pending.
+;;   (auto-package-update-maybe))
 
 ;; (use-package benchmark-init
 ;;   :ensure t
 ;;   :config
 ;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
+
+(defun get-last-modifed (file)
+  ;; source: https://gist.github.com/jmdeldin/4942461
+  "Returns the last modified date of a FILE."
+  (interactive)
+  (format-time-string "%Y-%m-%d %T"
+                      (nth 5 (file-attributes file))))
+
+(defun most-recent-p (file1 file2)
+  ;; source: https://gist.github.com/jmdeldin/4942461
+  "Returns t if FILE1 was modified more recently than FILE2."
+  (string< (get-last-modifed file1) (get-last-modifed file2)))
+
 
 (defun emacs_start__with_org()
   (require 'org)
@@ -12,18 +32,19 @@
    (expand-file-name "README.org"
                      user-emacs-directory)))
 
-(defun emacs_start__with_el()
+(defun emacs_start__with_tangled()
   (progn
-    ;; https://emacs.stackexchange.com/questions/29214/org-based-init-method-slows-down-emacs-startup-dramaticlly-6-minute-startup-h
     (defun my/tangle-dotfiles ()
+      ;; source: https://emacs.stackexchange.com/questions/29214/org-based-init-method-slows-down-emacs-startup-dramaticlly-6-minute-startup-h
       "If the current file is this file, the code blocks are tangled"
-      (when (equal (buffer-file-name) (expand-file-name "~/.emacs.d/README.org"))
-        (org-babel-tangle nil "~/.emacs.d/README.el")
+      (when (equal (buffer-file-name) (expand-file-name "~/.emacs.d_kadir/README.org"))
+        (org-babel-tangle nil "~/.emacs.d_kadir/README.el")
         ;; (byte-compile-file "~/.emacs.d/README.el")
         ))
     (add-hook 'after-save-hook #'my/tangle-dotfiles)
-    (load-file "~/.emacs.d/README.el")))
+    (load-file "~/.emacs.d_kadir/README.el")))
 
-(if (eq emacs_start_from nil)
-    (emacs_start__with_org)
-  (emacs_start__with_el))
+
+(if (most-recent-p  "~/.emacs.d_kadir/README.org" "~/.emacs.d_kadir/README.el")
+    (emacs_start__with_tangled)
+  (emacs_start__with_org))
