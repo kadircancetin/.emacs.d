65a489a00f8a6ee3eb26a05ead9af5630ef9c52b
kadir can çetin <kadircancetin@gmail.com>
2020-01-10 02:32:07 +0300

    Change opening progress

--- init.el
+++ init.el
@@ -1,38 +1,23 @@
-;; (use-package auto-package-update
-;;   :config
-;;   ;; Delete residual old versions
-;;   (setq auto-package-update-delete-old-versions t)
-;;   ;; Do not bother me when updates have taken place.
-;;   (setq auto-package-update-hide-results t)
-;;   ;; Update installed packages at startup if there is an update pending.
-;;   (auto-package-update-maybe))
-
 ;; (use-package benchmark-init
 ;;   :ensure t
 ;;   :config
 ;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar config-org (expand-file-name "README.org" user-emacs-directory))
+(defvar config-el (expand-file-name "README.el" user-emacs-directory))
 
 (defun emacs_start__with_org()
   (require 'org)
-  (org-babel-load-file
-   (expand-file-name "README.org"
-                     user-emacs-directory)))
+  (org-babel-load-file (expand-file-name config-org user-emacs-directory)))
 
-(defun emacs_start__with_tangled()
-  (progn
-    ;; (defun my/tangle-dotfiles ()
-    ;;   ;; source: https://emacs.stackexchange.com/questions/29214/org-based-init-method-slows-down-emacs-startup-dramaticlly-6-minute-startup-h
-    ;;   "If the current file is this file, the code blocks are tangled"
-    ;;   (when (equal (buffer-file-name) (expand-file-name "~/.emacs.d/README.org"))
-    ;;     (org-babel-tangle nil "~/.emacs.d/README.el")
-    ;;     ;; (byte-compile-file "~/.emacs.d/README.el")
-    ;;     ))
-    ;; (add-hook 'after-save-hook #'my/tangle-dotfiles)
-    (load-file "~/.emacs.d/README.el")
-    ))
+(defun emacs_start__with_tangled() (load-file config-el))
 
 
-(if (file-newer-than-file-p "~/.emacs.d/README.org" "~/.emacs.d/README.el")
-    (emacs_start__with_org)
-  (emacs_start__with_tangled))
+(if (file-exists-p config-el)
+    (if (file-newer-than-file-p config-org config-el)
+        (emacs_start__with_org)
+      (emacs_start__with_tangled))
+  (emacs_start__with_org))
