aa5389790672328ad676ee1cc3bf37a7ef33c190
kadir can çetin <kadircancetin@gmail.com>
2020-01-12 16:07:03 +0300

    [Fix] doesn't working early setting + [Add] package disable startup

--- early-init.el
+++ early-init.el
@@ -1,7 +1,6 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(blink-cursor-mode -1)
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
@@ -10,3 +9,5 @@
 (load-theme 'wombat t)
 (set-face-attribute 'default nil
                     :family "Source Code Pro" :height 80 :weight 'normal)
+
+(setq package-enable-at-startup nil)
