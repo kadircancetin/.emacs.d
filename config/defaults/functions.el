8ce68ce927c31a59d3d39049bc6aa3ef350ad2c4
kadir can çetin <kadircancetin@gmail.com>
2020-02-14 11:15:22 +0300

    Add delete-current-file function

--- config/defaults/functions.el
+++ config/defaults/functions.el
@@ -1,3 +1,16 @@
+(defun kadir/delete-file-and-buffer ()
+  ;; based on https://gist.github.com/hyOzd/23b87e96d43bca0f0b52 which
+  ;; is based on http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (when (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+          (delete-file filename)
+          (message "Deleted file %s." filename)
+          (kill-buffer))
+      (message "Not a file visiting buffer!"))))
+
 (defun eshell/clear ()
   "Clear the eshell buffer. Type clear on eshell"
   ;; source: https://emacs.stackexchange.com/questions/12503/how-to-clear-the-eshell
