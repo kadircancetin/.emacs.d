26a4089eed4064e58a1efa8e84bd97862cb079b0
kadir can çetin <kadircancetin@gmail.com>
2020-08-13 01:28:13 +0300

    Functional early init

--- early-init.el
+++ early-init.el
@@ -5,52 +5,54 @@
 (when kadir/emacs-fast-open (setq kadir/default-font-size 95))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; source: https://emacs.stackexchange.com/questions/34342/is-there-any-downside-to-setting-gc-cons-threshold-very-high-and-collecting-ga
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun k/set-garbage-collection()
+  "source: https://emacs.stackexchange.com/questions/34342/"
+  (setq load-prefer-newer noninteractive)
+  (defvar gc-cons-threshold-orginal (* 1024 1024 100))
+  (setq gc-cons-percentage 0.8)
+  (setq gc-cons-threshold most-positive-fixnum)
 
-(setq load-prefer-newer noninteractive)
-(defvar gc-cons-threshold-orginal (* 1024 1024 100))
-(setq gc-cons-percentage 0.8)
-(setq gc-cons-threshold most-positive-fixnum)
+  (defvar file-name-handler-alist-original file-name-handler-alist)
+  (setq file-name-handler-alist nil)
+  (run-with-idle-timer
+   3 nil
+   (lambda ()
+     (setq gc-cons-threshold gc-cons-threshold-orginal)
+     (setq file-name-handler-alist file-name-handler-alist-original)
+     (makunbound 'gc-cons-threshold-original)
+     (makunbound 'file-name-handler-alist-original)
+     (message "gc-cons-threshold and file-name-handler-alist restored"))))
 
-(defvar file-name-handler-alist-original file-name-handler-alist)
-(setq file-name-handler-alist nil)
-(run-with-idle-timer
- 3 nil
- (lambda ()
-   (setq gc-cons-threshold gc-cons-threshold-orginal)
-   (setq file-name-handler-alist file-name-handler-alist-original)
-   (makunbound 'gc-cons-threshold-original)
-   (makunbound 'file-name-handler-alist-original)
-   (message "gc-cons-threshold and file-name-handler-alist restored")))
+(defun k/set-init-package-disabling ()
+  (setq package-enable-at-startup nil)
+  ;; (setq load-prefer-newer t)
+  (setq load-prefer-newer noninteractive))
+
+(defun k/set-init-ui()
+  "Initialize UI. Mostly because of if theese settings in here, startup is faster"
+  (load-theme 'wombat t)
+  (fringe-mode 4)
+  (window-divider-mode 1)
+
+  (setq frame-inhibit-implied-resize t)              ;; doom says it increse speedup
+  (setq frame-resize-pixelwise t)                    ;; for terminal
+  (push '(menu-bar-lines . 0) default-frame-alist)   ;; (menu-bar-mode -1)
+  (push '(tool-bar-lines . 0) default-frame-alist)   ;; (tool-bar-mode -1)
+  (push '(vertical-scroll-bars) default-frame-alist) ;; (scroll-bar-mode -1)
+
+  (when (not kadir/emacs-fast-open)
+    (add-to-list 'default-frame-alist '(fullscreen . maximized)))
+
+  (setq window-divider-default-places t
+        window-divider-default-bottom-width 1
+        window-divider-default-right-width 1)
+  (set-face-attribute 'window-divider nil :foreground "#4C4262")
+
+  (set-face-attribute 'default nil :family "Source Code Pro" :height kadir/default-font-size :weight 'normal)
+  (set-face-attribute 'fixed-pitch-serif nil :family "Source Code Pro" :italic t :weight 'bold))
+
 
-(setq package-enable-at-startup nil)
-(advice-add #'package--ensure-init-file :override #'ignore)
-;; (setq load-prefer-newer t)       ; somehow dangers but when createing package, I need this
-(setq load-prefer-newer noninteractive)
 
-
-(load-theme 'wombat t)
-
-;; (menu-bar-mode -1)
-;; (tool-bar-mode -1)
-;; (scroll-bar-mode -1)
-(setq frame-inhibit-implied-resize t)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(fringe-mode 4)
-(when (not kadir/emacs-fast-open) (add-to-list 'default-frame-alist '(fullscreen . maximized)))
-(setq frame-resize-pixelwise t)
-
-(setq window-divider-default-places t
-      window-divider-default-bottom-width 1
-      window-divider-default-right-width 1)
-(set-face-attribute 'window-divider nil :foreground "#4C4262")
-(window-divider-mode 1)
-
-(set-face-attribute 'default nil :family "Source Code Pro" :height kadir/default-font-size :weight 'normal)
-(set-face-attribute 'fixed-pitch-serif nil :family "Source Code Pro" :italic t :weight 'bold)
-;; (set-face-attribute 'default nil :family "fira code" :height 75 :weight 'normal)
-;; (set-face-attribute 'default nil :family "Monoid" :height kadir/default-font-size :weight 'normal)
+(k/set-garbage-collection)
+(k/set-init-package-disabling)
+(k/set-init-ui)
