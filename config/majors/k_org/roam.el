5a6cde785b7602a8c5d03bc139cd2fe909a34b55
kadir <kadircancetin@gmail.com>
2020-09-20 12:31:55 +0300

    Fix org roam on native-compile

--- config/majors/k_org/roam.el
+++ config/majors/k_org/roam.el
@@ -1,27 +1,24 @@
 ;; org-roam
+(defun kadir/roam-hook()
+  (push 'company-org-roam company-backends))
+
 (use-package org-roam
-  :straight (
-             :no-native-compile t
-             ) ;; dont know why but native compile breaks the roam.
-  :defer nil
+  :straight (:no-native-compile t)
   :commands (org-roam org-roam-mode-map org-roam-find-file)
   :init
   (setq org-roam-buffer-width 0.25)
   (setq org-roam-buffer-no-delete-other-windows t)
   (setq org-roam-db-gc-threshold most-positive-fixnum)
   :hook
-  (org-mode . org-roam-mode)
   (org-roam-mode . kadir/roam-hook)
   :custom
   (org-roam-directory "~/Dropbox/org-roam/")
   :config
-  (require 'org-roam-protocol))
+  (require 'org-roam-protocol)
+  (org-roam-mode 1))
 
 (use-package company-org-roam
-  :init
-  (defun kadir/roam-hook()
-    (push 'company-org-roam company-backends))
-  )
+  :straight (:no-native-compile t))
 
 (use-package org-roam-server
-  :straight ( :no-native-compile t :no-byte-compile t))
+  :straight (:no-native-compile t))
