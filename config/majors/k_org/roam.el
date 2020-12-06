9eac6978f18cc0d1cf048d926c15a148c829332e
kadir <kadircancetin@gmail.com>
2020-12-06 22:13:44 +0300

    minor roam settings

--- config/majors/k_org/roam.el
+++ config/majors/k_org/roam.el
@@ -1,6 +1,11 @@
 ;; org-roam
 (defun kadir/roam-hook()
-  (push 'company-org-roam company-backends))
+  (push 'company-org-roam company-backends)
+
+  (setq jit-lock-defer-time nil
+        jit-lock-context-time 0.5
+        jit-lock-chunk-size 500
+        jit-lock-stealth-time nil))
 
 (use-package org-roam
   :straight (:no-native-compile t)
@@ -12,23 +17,22 @@
 
   :hook
   (org-mode . org-roam-mode)
+  (org-mode . kadir/force-roam-side-bar)
   (org-roam-mode . kadir/roam-hook)
 
   :config
-  (org-roam-buffer-activate)
 
-  (add-hook 'org-roam-backlinks-mode-hook 'kadir/darken-background)
+  (defun kadir/roam-sidebar-buffer-p ()
+    (and
+     org-roam-mode
+     (when (buffer-file-name) (s-contains? org-roam-directory (buffer-file-name)))
+     (not org-capture-mode)))
 
-  (add-to-list
-   'window-buffer-change-functions
-   '(lambda (n) (interactive)
-      (when (and
-             org-roam-mode
-             (or (derived-mode-p 'minibuffer-inactive-mode) ;; for helm
-                 (derived-mode-p 'org-mode))
-             (not org-capture-mode))
-        (org-roam-buffer-deactivate)
-        (org-roam-buffer-activate))))
+  (defun kadir/force-roam-side-bar (&rest n)
+    (interactive)
+    (when (kadir/roam-sidebar-buffer-p)
+      ;; (org-roam-buffer-deactivate)
+      (org-roam-buffer-activate)))
 
   (defun org-roam-protocol-open-file (info)
     (when-let ((file (plist-get info :file)))
@@ -38,9 +42,9 @@
       (org-roam--find-file file))
     nil)
 
-  (require 'org-roam-protocol)
   (defun kadir/open-org-roam-server-on-eaf()
     (interactive)
+    (require 'org-roam-protocol)
     (org-roam-server-mode 1)
     (kadir/load-eaf)
     (delete-other-windows)
@@ -48,8 +52,12 @@
     (resize-window-height 4)
     (eaf-open-browser "http://localhost:8080")
     (other-window 1)
-    (org-roam-buffer-activate)
-    ))
+    (org-roam-buffer-activate))
+
+  (add-hook 'org-roam-backlinks-mode-hook 'kadir/darken-background)
+  (add-to-list 'window-buffer-change-functions 'kadir/force-roam-side-bar)
+
+  )
 
 (use-package company-org-roam
   :straight (:no-native-compile t))
