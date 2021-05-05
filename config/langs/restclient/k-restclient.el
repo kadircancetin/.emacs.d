5d0cf51c5d46bb668d07a9fcf98372d2cd555ef2
Kadir Can Çetin <kadir-atlassian-qwm31a@deepinfo.com>
2021-05-05 13:03:39 +0300

    Fix restclient el

--- config/langs/restclient/k-restclient.el
+++ config/langs/restclient/k-restclient.el
@@ -1,11 +1,27 @@
-(defun kadir/restclient-send()
+(setq kadir/before-window nil)
+
+(defun kadir/rest-client-stay-current()
   (interactive)
-  (restclient-http-send-current-stay-in-window)
-  (sleep-for 1)
-  (other-window 1)
-  (json-mode-beautify)
-  (other-window -1)
-  (message "Attım geldim"))
+  (setq kadir/before-window (current-buffer))
+  (restclient-http-send-current-stay-in-window))
+
+(defun kadir/restclient-after-hook()
+  (interactive)
+
+  (let ((response-buf "*HTTP Response*"))
+    (when (get-buffer response-buf)
+      (pop-to-buffer response-buf)
+      (condition-case nil
+          (json-pretty-print-buffer)
+        (error nil)))))
+
+(defun kadir/select-before-window(&rest args)
+  (interactive)
+  (pop-to-buffer kadir/before-window))
+
+
+(add-hook 'restclient-response-loaded-hook 'kadir/restclient-after-hook)
+(advice-add 'restclient-http-handle-response :after 'kadir/select-before-window)
 
 (use-package restclient
   :commands (restclient-http-send-current restclient-http-send-current-stay-in-window)
@@ -14,7 +30,7 @@
   :config
   (setcdr (assoc "application/json" restclient-content-type-modes) 'json-mode)
   :bind ((:map restclient-mode-map
-               ("C-c C-c" . 'kadir/restclient-send)
+               ("C-c C-c" . 'kadir/rest-client-stay-current)
                ("C-c C-v" . 'restclient-http-send-current))))
 (use-package company-restclient
   :commands (json-mode-beautify)
