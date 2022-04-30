5fb6b310dd6b399ba690d0adae887c88e6801331
Kadir Can Çetin <kadircancetin@gmail.com>
2022-04-30 14:12:20 +0300

    Remove gitignore mode

--- config/langs/dotfiles/k-dotfiles.el
+++ config/langs/dotfiles/k-dotfiles.el
@@ -2,7 +2,7 @@
 (use-package docker-compose-mode :mode "docker-compose.*\.yml\\'")
 (use-package dockerfile-mode :mode "Dockerfile[a-zA-Z.-]*\\'")
 (use-package terraform-mode :mode ("\\.tf\\'" . terraform-mode))
-(use-package gitignore-mode :mode "/\\.gitignore\\'")
+;; (use-package gitignore-mode :mode "/\\.gitignore\\'")
 (use-package groovy-mode)
 (use-package jenkinsfile-mode)
 (use-package ansible)
